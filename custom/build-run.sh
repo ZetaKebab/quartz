51da3c172d82234d572aee6e14b18c2e957f3a24
Théo Marchal <me@theomarchal.com>
2024-04-06 18:38:57 +0200

    Add subtrees and scripts to build and update subtrees

--- custom/build-run.sh
+++ custom/build-run.sh
@@ -1,2 +1,2 @@
-docker build -t quartz-jpg ..
-docker-compose up -d --force-recreate
+docker build -t quartz-jpg .
+cd custom && docker-compose up -d --force-recreate
---
+++ custom/update-repos.sh
@@ -0,0 +1,5 @@
+git fetch convert-furigana main
+git subtree pull --prefix custom/convert-furigana convert-furigana main --squash
+
+git fetch convert-usage main
+git subtree pull --prefix custom/convert-usage convert-usage main --squash
