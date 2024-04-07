c6f9cd12e0f0602e3c06493988bdf0d013307851
Théo Marchal <me@theomarchal.com>
2024-04-08 00:10:18 +0200

    Added content repo in update script

--- custom/update-repos.sh
+++ custom/update-repos.sh
@@ -3,3 +3,6 @@ git subtree pull --prefix custom/convert-furigana convert-furigana main --squash
 
 git fetch convert-usage main
 git subtree pull --prefix custom/convert-usage convert-usage main --squash
+
+git fetch content main
+git subtree pull --prefix content content main --squash
